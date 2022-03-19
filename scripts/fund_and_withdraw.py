224ca0f73042f1d337c1abb6b972644264702199
zhojoe <zyj2000@gmail.com>
2022-03-19 09:54:26 +0800

    first commit

---
+++ scripts/fund_and_withdraw.py
@@ -0,0 +1,29 @@
+from brownie import FundMe
+from scripts.helpful_scripts import get_account
+from scripts.deploy import deploy_fund_me
+
+
+def fund():
+    print(len(FundMe))
+    if len(FundMe) <= 0:
+        fund_me = deploy_fund_me()
+    else:
+        fund_me = FundMe[-1]
+
+    account = get_account()
+    entrance_fee = fund_me.getEntranceFee()
+    print(entrance_fee)
+    print(f"The current entry fee is {entrance_fee}")
+    print("Funding...")
+    fund_me.fund({"from": account, "value": entrance_fee})
+
+
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    fund_me.withdraw({"from": account})
+
+
+def main():
+    fund()
+    # withdraw()
